de0870be4d0969e3cbabeeffe64d64d2a4e5693a
Michael Yagodarov <purposelessness@yandex.ru>
2023-04-04 18:28:45 +0300

    refactor(prefix_function): initializing j only once

--- modules/prefix_function.py
+++ modules/prefix_function.py
@@ -1,7 +1,7 @@
 def prefix_function(s) -> list[int]:
     pi = [0] * len(s)
+    j = 0
     for i in range(1, len(s)):
-        j = pi[i - 1]
         while j > 0 and s[i] != s[j]:
             j = pi[j - 1]
         if s[i] == s[j]:
